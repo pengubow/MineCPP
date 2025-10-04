cfdf43d202131572fec6b10705fa6d6bf8e32221
pengubow <remecedik@gmail.com>
2025-10-04 11:32:00 +0300

    whoops comm and server didnt delete wth

--- comm/ServerListener.h
+++
@@ -1,8 +0,0 @@
-#include <exception>
-#include "comm/SocketConnection.h"
-
-class ServerListener {
-public:
-    virtual void clientConnected(SocketConnection* var1) = 0;
-    virtual void clientException(SocketConnection* var1, const std::exception& var2) = 0;
-};
